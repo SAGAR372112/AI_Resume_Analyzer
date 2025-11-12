f4666c137c39ad04d27f0790616ac5fc62cf2b66
Sagar <sagarnanera372@gmail.com>
2025-11-12 12:29:43 +0530

    add corsheader and allowed origin

--- core/settings.py
+++ core/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'resume_analyzer',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,7 +53,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "https://sagar372112-resume-analyzer-frontend-app-cuunze.streamlit.app",
+]
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
